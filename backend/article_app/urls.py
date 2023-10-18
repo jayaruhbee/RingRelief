585b75618aa8e81fefa995680259eb5772b3c207
jayaruhbee <ottoman-fruity.0m@icloud.com>
2023-10-18 10:21:51 -0400

    views for filtered Articles

--- backend/article_app/urls.py
+++ backend/article_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ResearchArticle, TopicToKeywords
+from .views import ResearchArticles, TopicToKeyword, FilteredArticles, KeywordsSet
 
 urlpatterns = [
-    path('', ResearchArticle.as_view(), name='all_articles'),
-    path('keywords/', TopicToKeywords.as_view(), name='all_keywords'),
+    path('', ResearchArticles.as_view(), name='all_articles'),
+    path('filter/<str:keyword>/', FilteredArticles.as_view(), name='filtered_articles'),
+    path('keywords/', TopicToKeyword.as_view(), name='all_keywords'),
+    path('keyword_set/', KeywordsSet.as_view(), name='set_keywords'),
 ]
